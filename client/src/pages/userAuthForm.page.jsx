c78d4d09ee2809c37447e6ddfb9d8d29b37d4f6c
MilanMItra2210 <milanmitranda2020@gmail.com>
2024-03-31 18:40:17 +0530

    Initial Project setup

---
+++ client/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserAuthForm = ({type}) => {
+  return (
+    <h1>{type}</h1>
+  )
+}
+
+export default userAuthForm
---
+++ client/vite.config.js
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0'
+  }
+};
